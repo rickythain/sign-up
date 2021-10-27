5b318e951456fff28d3411ba1fb99b7bcb702fc8
Ricky Thain <rickythain@gmail.com>
2021-10-27 23:50:35 +0800

    complete initial exercise functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sign Up',
-      theme: ThemeData(
-        primarySwatch: Colors.lime,
+      theme: new ThemeData(
+        scaffoldBackgroundColor: Colors.white,
       ),
-      home: SignUpPage(),
+      home: SignUpPage(
+      ),
     );
   }
 }
@@ -25,11 +26,119 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+
+  List<String> genders = ['male', 'female', 'other', 'rather not say'];
+  String? _gender;
+
+  final textControllerName = TextEditingController();
+  final textControllerEmail = TextEditingController();
+  final textControllerPassword = TextEditingController();
+
+  bool isDisbled = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Start listening to changes.
+    textControllerName.addListener(isFilled);
+    textControllerEmail.addListener(isFilled);
+    textControllerPassword.addListener(isFilled);
+  }
+
+   @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the
+    // widget tree.
+    textControllerName.dispose();
+    textControllerEmail.dispose();
+    textControllerPassword.dispose();
+    super.dispose();
+  }
+
+  void isFilled() {
+    setState(() {
+      if (textControllerName.text != "" && textControllerEmail.text != "" && textControllerPassword.text != "" && _gender != null) {
+      isDisbled = false;
+    } else {
+      isDisbled = true;
+    }
+
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Column(
-      children: [Image.network("https://blog.deriv.com/content/images/2021/04/derive-logo-01.png")],
-    ),);
+    return Scaffold(
+      body: Center(
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+            Text("Welcome to Deriv", style: TextStyle(color: Colors.black, fontSize: 20),),
+            Image.network("https://tradingbrokers.com/wp-content/uploads/2020/11/Deriv-Logo.png"),
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'Name',
+                border: OutlineInputBorder(),
+              ),
+              controller: textControllerName,
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                hintText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+              controller: textControllerEmail,
+            ),
+            TextField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                hintText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+              controller: textControllerPassword,
+            ),
+            Container(
+              child: DropdownButton<String>(
+                value: _gender,
+                
+                items: <String>[
+                  'male',
+                  'female',
+                  'other',
+                  'rather not say'
+                ].map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                    );
+                }).toList(),
+
+                hint: Text(
+                  "Select a gender"
+                ),
+                
+                onChanged: (String? value) {
+                  setState(() {
+                    _gender = value;
+                  });
+                },
+                
+                ),
+              ),
+            ElevatedButton(
+              onPressed: isDisbled ? null : () {
+                final snackBar = SnackBar(
+                  content: const Text('You have signed up successfully! :)'),
+                  );
+                  
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              }, 
+              child: const Text('Sign up'),
+              ),
+          ],
+    ),),);
+   
   }
 }
 
